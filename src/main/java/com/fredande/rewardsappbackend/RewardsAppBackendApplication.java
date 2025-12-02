866a6b0d91972dcefcef2d6d28c4c4fada22a73d
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-02 14:58:49 +0100

    Initial commit

---
+++ src/main/java/com/fredande/rewardsappbackend/RewardsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fredande.rewardsappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fredande/rewardsappbackend/RewardsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredande.rewardsappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
