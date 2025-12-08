b0e666382be5458ef04b22d15234265b58d5e099
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-08 13:25:14 +0100

    Add RegistrationRequest DTO

---
+++ src/main/java/com/fredande/rewardsappbackend/dto/RegistrationRequest.java
@@ -0,0 +1,36 @@
+package com.fredande.rewardsappbackend.dto;
+
+import jakarta.validation.constraints.*;
+
+public class RegistrationRequest {
+
+    private final int minPasswordLength = 8;
+    private final int maxPasswordLength = 40;
+    private final String emailRegEx = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+
+    @Pattern(regexp = emailRegEx,
+            message = "Malformed email address")
+    private String email;
+    @Size(min = minPasswordLength,
+            max = maxPasswordLength,
+            message = "Password must be " + minPasswordLength + " to " + maxPasswordLength + " characters")
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
