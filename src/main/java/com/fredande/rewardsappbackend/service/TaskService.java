c33b20b7d9b2a2b82d2081487c6448bd7a2c48ed
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-10 15:34:12 +0100

    Add create and get methods

--- src/main/java/com/fredande/rewardsappbackend/service/TaskService.java
+++ src/main/java/com/fredande/rewardsappbackend/service/TaskService.java
@@ -1,25 +1,45 @@
 package com.fredande.rewardsappbackend.service;
 
+import com.fredande.rewardsappbackend.CustomUserDetails;
+import com.fredande.rewardsappbackend.dto.TaskCreationRequest;
 import com.fredande.rewardsappbackend.dto.TaskSavedResponse;
 import com.fredande.rewardsappbackend.model.Task;
+import com.fredande.rewardsappbackend.model.User;
 import com.fredande.rewardsappbackend.repository.TaskRepository;
-import jakarta.validation.Valid;
+import com.fredande.rewardsappbackend.repository.UserRepository;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
 
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, UserRepository userRepository) {
         this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
     }
 
-
-    public TaskSavedResponse create(@Valid Task task) {
-        TaskSavedResponse taskSavedResponse = new TaskSavedResponse(task.getTitle(), task.getDescription());
+    public TaskSavedResponse create(TaskCreationRequest taskCreationRequest, CustomUserDetails userDetails) {
+        User user = new User();
+        user.setId(userDetails.getId());
+        TaskSavedResponse taskSavedResponse = new TaskSavedResponse(taskCreationRequest.getTitle(), taskCreationRequest.getDescription());
+        Task task = new Task();
+        task.setTitle(taskCreationRequest.getTitle());
+        task.setDescription(taskCreationRequest.getDescription());
+        task.setPoints(taskCreationRequest.getPoints());
+        task.setUser(user);
         taskRepository.save(task);
         return taskSavedResponse;
+    }
+
+    public @Nullable List<Task> getTasksByUser(CustomUserDetails userDetails) {
+        User user = userRepository.findById(userDetails.getId()).orElseThrow();
+        List<Task> tasks = taskRepository.findByUser(user).orElse(null);
+        return tasks;
     }
 
 }
