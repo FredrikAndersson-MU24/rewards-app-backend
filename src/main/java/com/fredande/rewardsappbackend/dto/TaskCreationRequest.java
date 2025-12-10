e146378e03b246b4e340343fb4bd1eb248236bf4
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-10 15:35:27 +0100

    Add dto for task creation request

---
+++ src/main/java/com/fredande/rewardsappbackend/dto/TaskCreationRequest.java
@@ -0,0 +1,36 @@
+package com.fredande.rewardsappbackend.dto;
+
+public class TaskCreationRequest {
+
+    private String title;
+    private String description;
+    private Integer points;
+
+    public TaskCreationRequest() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+}
