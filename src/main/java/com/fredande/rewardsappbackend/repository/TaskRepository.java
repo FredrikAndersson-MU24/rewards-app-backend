6e6cf2be8a28f1082136b767255b8f800f23ea17
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-10 15:35:08 +0100

    Add findByUser methoid

--- src/main/java/com/fredande/rewardsappbackend/repository/TaskRepository.java
+++ src/main/java/com/fredande/rewardsappbackend/repository/TaskRepository.java
@@ -1,10 +1,15 @@
 package com.fredande.rewardsappbackend.repository;
 
 import com.fredande.rewardsappbackend.model.Task;
+import com.fredande.rewardsappbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    List<Task> findByUser(User user);
 
 }
