874f9f3a20de57fd8bf05f8d3df111b35b67e19b
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-15 16:33:46 +0100

    Add id to the signature

--- src/main/java/com/fredande/rewardsappbackend/dto/TaskReadResponse.java
+++ src/main/java/com/fredande/rewardsappbackend/dto/TaskReadResponse.java
@@ -4,14 +4,16 @@ import org.springframework.web.util.HtmlUtils;
 
 import java.util.Date;
 
-public record TaskReadResponse(String title,
+public record TaskReadResponse(Integer id,
+                               String title,
                                String description,
                                Integer points,
                                Date created,
                                Date updated,
                                boolean done) {
 
-    public TaskReadResponse(String title, String description, Integer points, Date created, Date updated, boolean done) {
+    public TaskReadResponse(Integer id, String title, String description, Integer points, Date created, Date updated, boolean done) {
+        this.id = id;
         this.title = HtmlUtils.htmlEscape(title);
         this.description = HtmlUtils.htmlEscape(description);
         this.points = points;
