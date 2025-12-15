b9d6053de8c2cc298e1599b9b38df014b59c6ff8
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-15 16:33:51 +0100

    Add id to the signature

--- src/main/java/com/fredande/rewardsappbackend/dto/TaskSavedResponse.java
+++ src/main/java/com/fredande/rewardsappbackend/dto/TaskSavedResponse.java
@@ -2,9 +2,10 @@ package com.fredande.rewardsappbackend.dto;
 
 import org.springframework.web.util.HtmlUtils;
 
-public record TaskSavedResponse(String title, String description, Integer points) {
+public record TaskSavedResponse(Integer id, String title, String description, Integer points) {
 
-    public TaskSavedResponse(String title, String description, Integer points) {
+    public TaskSavedResponse(Integer id, String title, String description, Integer points) {
+        this.id = id;
         this.title = HtmlUtils.htmlEscape(title);
         this.description = HtmlUtils.htmlEscape(description);
         this.points = points;
