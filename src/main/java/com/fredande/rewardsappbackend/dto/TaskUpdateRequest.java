c5d08fe3c8f45e2b3f47da0436195984dbc54293
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-15 16:34:09 +0100

    MAke fields nullable to allow partial update

--- src/main/java/com/fredande/rewardsappbackend/dto/TaskUpdateRequest.java
+++ src/main/java/com/fredande/rewardsappbackend/dto/TaskUpdateRequest.java
@@ -1,5 +1,20 @@
 package com.fredande.rewardsappbackend.dto;
 
-public record TaskUpdateRequest(String title, String description, Integer points, boolean done) {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.jspecify.annotations.Nullable;
+
+public record TaskUpdateRequest(@Size(min = 8, max = 140, message = "Title must be 8-40 characters.")
+                                @Nullable
+                                String title,
+                                @Size(min = 8, max = 255, message = "Description must be 8-255 characters.")
+                                @Nullable
+                                String description,
+                                @Min(value = 0, message = "Points must be a whole number of zero or greater")
+                                @Nullable
+                                Integer points,
+                                @Nullable
+                                Boolean done) {
 
 }
