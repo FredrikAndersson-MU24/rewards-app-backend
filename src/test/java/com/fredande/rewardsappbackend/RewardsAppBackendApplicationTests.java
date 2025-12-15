2fd1d3df125f2ad50a68c14af3805d2035d03834
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-15 22:33:59 +0100

    rename job

--- src/test/java/com/fredande/rewardsappbackend/RewardsAppBackendApplicationTests.java
+++ src/test/java/com/fredande/rewardsappbackend/RewardsAppBackendApplicationTests.java
@@ -1,13 +1,15 @@
 package com.fredande.rewardsappbackend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class RewardsAppBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
