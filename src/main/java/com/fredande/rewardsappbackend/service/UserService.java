7e78947a00616bb1a6a66267edda507a276253ad
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-16 11:02:45 +0100

    Handle points when rask done/not done

--- src/main/java/com/fredande/rewardsappbackend/service/UserService.java
+++ src/main/java/com/fredande/rewardsappbackend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.fredande.rewardsappbackend.service;
 
+import com.fredande.rewardsappbackend.model.User;
 import com.fredande.rewardsappbackend.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,17 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public void updatePoints(Integer userId, Integer points, boolean done) {
+        User savedUser = userRepository.findById(userId).orElseThrow();
+        if (done) {
+            savedUser.setCurrentPoints(savedUser.getCurrentPoints() + points);
+            savedUser.setTotalPoints(savedUser.getTotalPoints() + points);
+        } else {
+            savedUser.setCurrentPoints(savedUser.getCurrentPoints() - points);
+            savedUser.setTotalPoints(savedUser.getTotalPoints() - points);
+        }
     }
 
 
