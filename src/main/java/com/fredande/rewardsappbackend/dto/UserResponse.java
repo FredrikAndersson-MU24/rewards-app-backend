b6914ff4fa2b3ecac2ccfad2c7e22e14c159d855
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-12-16 14:10:17 +0100

    Remove constructor

--- src/main/java/com/fredande/rewardsappbackend/dto/UserResponse.java
+++ src/main/java/com/fredande/rewardsappbackend/dto/UserResponse.java
@@ -1,7 +1,5 @@
 package com.fredande.rewardsappbackend.dto;
 
-import org.springframework.web.util.HtmlUtils;
-
 public record UserResponse(String email,
                            String firstName,
                            String lastName,
@@ -11,24 +9,5 @@ public record UserResponse(String email,
                            Integer numTasksCompleted,
                            Integer numTasksTotal) {
 
-    public UserResponse(String email,
-                        String firstName,
-                        String lastName,
-                        Integer currentPoints,
-                        Integer totalPoints,
-                        Integer numTasksOpen,
-                        Integer numTasksCompleted,
-                        Integer numTasksTotal) {
-        this.email = email;
-        this.firstName = HtmlUtils.htmlEscape(firstName);
-        this.lastName = HtmlUtils.htmlEscape(lastName);
-        this.currentPoints = currentPoints;
-        this.totalPoints = totalPoints;
-        this.numTasksOpen = numTasksOpen;
-        this.numTasksCompleted = numTasksCompleted;
-        this.numTasksTotal = numTasksTotal;
-
-
-    }
 
 }
